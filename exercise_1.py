d8a9628da2713eba9ef7be51cee69883672f218f
mytab-0914 <mytab0914@gmail.com>
2020-01-18 19:28:24 +0100

    my first repository

---
+++ exercise_1.py
@@ -0,0 +1,17 @@
+# Exercise 1
+# source: https://www.practicepython.org/
+from datetime import date
+
+name = input("Tell me your name: ")
+age = int(input("Now, tel me your age: "))
+year = datetime.year()
+print("Accordin to the given age I estimaded that you were born in "...)
+
+date = 
+
+choose = input("Would you like me to print it? Y/N")
+
+if choose == "Y":
+    print(year)
+elif choose == "N":
+    print("That is OK. The program will terminate after displaing this message")
